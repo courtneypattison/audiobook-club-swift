fd4a453a7cf4e8a930ca459e59425762bb833865
courtneypattison <courtney@courtneypattison.com>
2017-05-31 08:24:18 -0400

    issue-5: added AudiobookTableViewCell

---
+++ AudiobookClub/View/AudiobookTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  AudiobookTableViewCell.swift
+//  AudiobookClub
+//
+//  Created by Courtney Pattison on 2017-05-31.
+//  Copyright © 2017 Courtney Pattison. All rights reserved.
+//
+
+import UIKit
+
+class AudiobookTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var audiobookTitleLabel: UILabel!
+    @IBOutlet weak var audiobookAuthorsLabel: UILabel!
+    @IBOutlet weak var audiobookRatingLabel: UILabel!
+    @IBOutlet weak var audiobookImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
