dd9d64c9c5d123bbdc32a38a0d0b53c7f7b1ceea
courtneypattison <courtney@courtneypattison.com>
2017-06-05 15:15:33 -0400

    issue-29: removed hard coded loading of audiobooks

--- AudiobookClub/Controller/AudiobookTableViewController.swift
+++ AudiobookClub/Controller/AudiobookTableViewController.swift
@@ -32,7 +32,6 @@ class AudiobookTableViewController: UITableViewController {
             audiobookViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? AudiobookViewController
         }
         
-        //loadSampleAudiobooks()
         loadAudiobooks()
     }
 
@@ -140,39 +139,5 @@ class AudiobookTableViewController: UITableViewController {
             }
         }
     }
-    
-    func loadSampleAudiobooks() {
-        guard let audiobook1 = Audiobook(identifier: "jane_eyre_ver03_0809_librivox",
-                                         title: "Jane Eyre (version 3)",
-                                         authors: ["Charlotte Brontë"],
-                                         description: "<a href=\"http://librivox.org/\" rel=\"nofollow\">LibriVox</a> recording of Jane Eyre, by Charlotte Brontë. Read by Elizabeth Klett.\n\n" +
-                                            "Charlotte Bronte's classic novel Jane Eyre is narrated by the title character, an orphan who survives neglect and abuse to become a governess at the remote Thornfield Hall. She finds a kindred spirit in her employer, the mysterious and brooding Mr. Rochester, but he hides a terrible secret that threatens their chances of happiness. (Summary by Elizabeth Klett)\n\n" +
-                                            "For further information, including links to online text, reader information, RSS feeds, CD cover or other formats (if available), please go to the LibriVox <a href=\"https://librivox.org/jane-eyre-version-3-by-charlotte-bronte/\" rel=\"nofollow\">catalog page</a> for this recording.\n\n" +
-                                            "For more free audio books or to become a volunteer reader, visit <a href=\"http://librivox.org/\" rel=\"nofollow\">LibriVox.org</a>.\n\n" +
-                                            "<a href=\"https://archive.org/download/jane_eyre_ver03_0809_librivox/JaneEyrepart1_librivox.m4b\" rel=\"nofollow\">Download M4B Part 1 (255MB)</a>\n" +
-            "<a href=\"https://archive.org/download/jane_eyre_ver03_0809_librivox/JaneEyrepart2_librivox.m4b\" rel=\"nofollow\">Download M4B Part 2 (273MB)</a>\n",
-                                         subjects: "librivox; audiobook; classic; romance;",
-                                         runtime: "18:36:29",
-                                         rating: 5.00,
-                                         imageURL: URL(string: "https://ia802702.us.archive.org/18/items/jane_eyre_ver03_0809_librivox/Jane_Eyre_1002_thumb.jpg"),
-                                         chapters: [URL(string: "https://archive.org/download/jane_eyre_ver03_0809_librivox/janeeyre_01_bronte.mp3")!]) else {
-            fatalError("Unable to instantiate audiobook1")
-        }
-        
-        guard let audiobook2 = Audiobook(identifier: "bonnie_prince_charlie_1503_librivox",
-                                         title: "",
-                                         authors: ["Shenzi", "Raj"],
-                                         description: "Cats are the coolest",
-                                         subjects: "cats;",
-                                         runtime: "10:36:20",
-                                         imageURL: URL(string: ""),
-                                         chapters: [URL(string: "https://archive.org/download/jane_eyre_ver03_0809_librivox/janeeyre_01_bronte.mp3")!]) else {
-                                            fatalError("Unable to instantiate audiobook2")
-        }
-        
-        audiobooks.append(audiobook1)
-        audiobooks.append(audiobook2)
-    }
-
 }
 
