f7156dd641bc791e0bde2a661a68e16d51cb1919
courtneypattison <courtney@courtneypattison.com>
2017-06-09 08:31:07 -0400

    [50] added chapter struct

---
+++ AudiobookClub/Model/Chapter.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter.swift
+//  AudiobookClub
+//
+//  Created by Courtney Pattison on 2017-06-09.
+//  Copyright © 2017 Courtney Pattison. All rights reserved.
+//
+
+import Foundation
+
+struct Chapter {
+    
+    // Mark: Properties
+    
+    let url: URL
+    let title: String
+}
